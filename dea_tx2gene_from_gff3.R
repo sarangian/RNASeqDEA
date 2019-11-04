3fba1c5e4fdff5805613bdd2f137088d7e926a9e
Aditya Narayan Sarangi <a.n.sarangi@gmail.com>
2019-11-04 16:00:04 +0530

    Add files via upload

---
+++ dea_tx2gene_from_gff3.R
@@ -0,0 +1,30 @@
+#!/usr/bin/env Rscript
+suppressMessages(library(readr))
+suppressMessages(library(optparse))
+suppressMessages(library(tximport))
+suppressMessages(library(GenomicFeatures))
+
+option_list = list(
+  make_option(c("-g", "--gff3"), type="character", default=NULL,help="gff3 file path", metavar="character"),
+  make_option(c("-o", "--out"), type="character", default=NULL,help="tx2gene file name (.tsv)", metavar="character")
+ );
+
+
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser);
+
+txdb <- makeTxDbFromGFF(file=opt$gff3)
+
+txdf <- select(txdb, keys(txdb, "TXNAME"), "GENEID", "TXNAME")
+#tab <- select(txdb, keys(txdb, "TXNAME"), "TXNAME")
+#txdf$ntx <- tab[match(txdf$GENEID, names(tab))]
+
+
+txdf$GENEID <-paste(txdf$TXNAME,txdf$GENEID, sep = "*")
+
+
+head(txdf)
+write.table(txdf,opt$out, sep=",", col.names = TRUE, row.names = FALSE, quote=FALSE)
+
+
+
